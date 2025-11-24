4b3162a275088886e1c593734d005e756f71c5b6
kminbaeva <kminbaeva@hof-university.de>
2025-11-24 21:51:33 +0100

    Initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -46,8 +46,8 @@ const AppManagementAdminPage = lazy(() => import('./pages/admin/appManagementAdm
 const AppWrapper: React.FC = () => (
     <ConfigProvider locale={customLocale} theme={customTheme}>
         <AntdApp>
-        {/* <Router> */}
-        <Router basename="/dilowa">
+        <Router>
+        {/* <Router basename="/dilowa"> */}
             <Layout className="app-layout">
                 <div className="app-container">
                     <AppHeader />
